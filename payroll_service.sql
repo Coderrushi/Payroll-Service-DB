fa75fb560a4a960f2d4bbba4f1a272d5d066621e
Hrushikesh Zarekar <rushikeshzarekar265@gmail.com>
2024-11-04 16:14:09 +0530

    created table employee_payroll

--- payroll_service.sql
+++ payroll_service.sql
@@ -3,3 +3,10 @@ create database payroll_service;
 select name from sys.databases; --to see available databases
 
 use payroll_service;
+
+Create Table employee_payroll (
+	empId int identity(1,1) PRIMARY KEY,
+	empName varchar(50) not null,
+	empSalary decimal(10,2) not null,
+	startDate date not null
+);
